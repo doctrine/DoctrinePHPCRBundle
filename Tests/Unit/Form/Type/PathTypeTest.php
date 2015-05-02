a77082e5569983213e4ca345a4516b364258ff4b
David Buchmann <david.buchmann@liip.ch>
2015-05-02 18:53:22 +0200

    test minimal version and adjust composer.json to force new enough versions

--- Tests/Unit/Form/Type/PathTypeTest.php
+++ Tests/Unit/Form/Type/PathTypeTest.php
@@ -2,12 +2,39 @@
 
 namespace Doctrine\Bundle\PHPCRBundle\Tests\Unit\Form\Type;
 
-use Doctrine\Bundle\PHPCRBundle\Form\DataTransformer\PHPCRNodeToPathTransformer;
-use Jackalope\Node;
+use Doctrine\Bundle\PHPCRBundle\ManagerRegistry;
 use Doctrine\Bundle\PHPCRBundle\Form\Type\PathType;
+use Doctrine\ODM\PHPCR\DocumentManager;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PathTypeTest extends \PHPUnit_Framework_Testcase
 {
+    /**
+     * @var ManagerRegistry|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $reg;
+
+    /**
+     * @var DocumentManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $dm;
+
+    /**
+     * @var FormBuilder|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $builder;
+
+    /**
+     * @var OptionsResolver|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $optionsResolver;
+
+    /**
+     * @var PathType
+     */
+    private $type;
+
     public function setUp()
     {
         $this->reg = $this->getMockBuilder('Doctrine\Bundle\PHPCRBundle\ManagerRegistry')
@@ -53,4 +80,3 @@ class PathTypeTest extends \PHPUnit_Framework_Testcase
         $this->type->setDefaultOptions($this->optionsResolver);
     }
 }
-
