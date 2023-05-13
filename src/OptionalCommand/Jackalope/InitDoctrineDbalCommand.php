a7c7924e76afbc19cffda5dbbf221f6da1467b37
David Buchmann <david.buchmann@liip.ch>
2023-05-13 09:50:20 +0200

    declare return types on commands to be future proof

--- src/OptionalCommand/Jackalope/InitDoctrineDbalCommand.php
+++ src/OptionalCommand/Jackalope/InitDoctrineDbalCommand.php
@@ -4,16 +4,15 @@ namespace Doctrine\Bundle\PHPCRBundle\OptionalCommand\Jackalope;
 
 use Doctrine\Bundle\PHPCRBundle\Command\DoctrineCommandHelper;
 use Jackalope\Tools\Console\Command\InitDoctrineDbalCommand as BaseInitDoctrineDbalCommand;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Wrapper to use this command in the symfony console with multiple sessions.
+ */
 class InitDoctrineDbalCommand extends BaseInitDoctrineDbalCommand
 {
-    /**
-     * @see Command
-     */
     protected function configure(): void
     {
         parent::configure();
@@ -24,9 +23,6 @@ class InitDoctrineDbalCommand extends BaseInitDoctrineDbalCommand
         ;
     }
 
-    /**
-     * @see Command
-     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $application = $this->getApplication();
